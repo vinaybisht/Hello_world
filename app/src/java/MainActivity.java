5ec6a42d98fda327d57ac465c6403af0dc23b4ba
vinay singh bisht <bishtvinay143@gmail.com>
2016-09-06 13:16:59 +0530

    Basic Android App
    This is the very basic android app for beginners

---
+++ app/src/java/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.vinaybisht.helloworld;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "My First Android App", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+}
